a45ba8d8b3e7c5f3ed9b0ca08da95be93d2785cc
hyphen <lhfcjhyy@gmail.com>
2024-02-19 01:37:52 +0800

    update

--- lib/qml_engine.cpp
+++ lib/qml_engine.cpp
@@ -2,38 +2,40 @@
 #include "interceptor.h"
 #include <qstringliteral.h>
 #include <qurl.h>
+#include<qdir.h>
 QmlEngine::QmlEngine() {
-  auto &interceptor = Interceptor::get_instance();
-  interceptor.AddWatchFile = [this](QString url) {
-    qDebug() << "add file:" << url;
-    this->watcher_.AddFile(url);
-  };
-  watcher_.CleanEngine = [this]() {
-    qDebug() << "clear engine";
-    this->trimComponentCache();
-    this->clearComponentCache();
-    auto *obj = this->rootObjects().at(0);
-    QMetaObject::invokeMethod(obj, "reload");
-  };
-  this->addUrlInterceptor(&interceptor);
+	auto& interceptor = Interceptor::get_instance();
+	interceptor.AddWatchFile = [this](QString url) {
+		qDebug() << "add file:" << url;
+		this->watcher_.AddFile(url);
+		};
+	watcher_.CleanEngine = [this]() {
+		qDebug() << "clear engine";
+		this->trimComponentCache();
+		this->clearComponentCache();
+		auto* obj = this->rootObjects().at(0);
+		QMetaObject::invokeMethod(obj, "reload");
+		};
+	this->addUrlInterceptor(&interceptor);
 }
 
-QmlEngine::QmlEngine(QObject *parent) : QQmlApplicationEngine(parent) {}
+QmlEngine::QmlEngine(QObject* parent) : QQmlApplicationEngine(parent) {}
 
-QmlEngine &QmlEngine::get_instance() {
-  static QmlEngine engine;
-  return engine;
+QmlEngine& QmlEngine::get_instance() {
+	static QmlEngine engine;
+	return engine;
 }
 
-void QmlEngine::loadRelativePath(const QString &filePath) {
-  qDebug()<<"load file:"<<filePath;
-  QUrl url;
+void QmlEngine::loadRelativePath(const QString& filePath) {
+	qDebug() << "load file:" << filePath;
+	QUrl url;
 #ifdef DEBUG
-  qDebug() << "debug mode";
-  url = project_root_ + QString("/") + filePath;
+	qDebug() << "debug mode";
+	url = QUrl::fromLocalFile(project_root_ + QDir::separator() + filePath);
 #else
-  qDebug() << "release mode";
-  url = QString("qrc:/") + filePath;
+	qDebug() << "release mode";
+	url = QString("qrc:/") + filePath;
 #endif
-  QQmlApplicationEngine::load(url);
+	//url = QString("file:///c:/work/QtHotReloadTemplate/main.qml");
+	QQmlApplicationEngine::load(url);
 }
